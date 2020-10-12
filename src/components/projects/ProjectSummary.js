e3e4c12d4f71b1b6301d025811382616e2d4164e
Alejandro Aragon <ing.aragon.ipn@gmail.com>
2020-10-12 18:46:19 -0500

    project summary component with dummy data

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ProjectSummary() {
+  return (
+    <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">
+            Titulo del Proyecto
+          </span>
+          <p>Posteado por default dummy data</p>
+          <p className="grey-text">12 de Octubre</p>
+        </div>
+      </div>
+  )
+}
