c1044055a8d45f3c0e21f9d1cb6c7194da9aeae2
Alejandro Aragon <ing.aragon.ipn@gmail.com>
2020-10-12 22:49:35 -0500

    thunk

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,11 +1,16 @@
 const initState = {
   projects:[
     { id:'1',title:'do something',content:'details for do something blah blah blah' },
-    { id:'1',title:'do something',content:'details for do something blah blah blah' },
-    { id:'1',title:'do something',content:'details for do something blah blah blah' }
+    { id:'2',title:'do something',content:'details for do something blah blah blah' },
+    { id:'3',title:'do something',content:'details for do something blah blah blah' }
   ]
 }
 const projectReducer = (state = initState, action) => {
+  switch(action.type){
+    case 'CREATE_PROJECT': 
+      console.log('created project', action.project);
+      break;
+  }
   return state;
 }
 export default projectReducer;
